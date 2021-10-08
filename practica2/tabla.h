6639e81460ad1c6164648e460f408694801a36c8
Francisco Jose Aparicio Martos <paco2000am@gmail.com>
2021-10-08 22:05:00 +0200

    Primera version contiene fallos

---
+++ practica2/tabla.h
@@ -0,0 +1,38 @@
+#ifndef _TABLA_H_INCLUDED
+#define TABLA_H_INCLUDED
+
+#define PRINCIPAL 257
+#define LLAVEIZQ 258
+#define LLAVEDER 259
+#define COMA 260
+#define LISTADE 261
+#define PROCEDIMIENTO 262
+#define PARDER 263
+#define PARIZQ 264
+#define CORCHIZQ 265
+#define CORCHDER 266
+#define IGUAL 267
+#define SI 268
+#define MIENTRAS 269
+#define OTROCASO 270
+#define PARA 271
+#define HASTA 272
+#define HACER 273
+#define LEER 274
+#define IMPRIMIR 275
+#define CADENA 276
+#define ID 277
+#define RETROCEDER 278
+#define DOLLAR 279
+#define PYC 280
+#define OP1 281
+#define OP2 282
+#define OP3 283
+#define OP4 284
+#define OP5 285
+#define OP6 286
+#define TIPOS 287
+#define CONSTANTE 288
+#define ESPACIO 289
+
+#endif
