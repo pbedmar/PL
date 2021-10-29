b06d8aecee7b8f09d790e01628659f84401a2292
Pedro Bedmar Lopez <correo.pedrobedmar@gmail.com>
2021-10-29 13:15:46 +0200

    compiling error doesn't appear anymore on VSCode

--- practica3/src/y.tab.h
+++ practica3/src/y.tab.h
@@ -1,14 +1,14 @@
-/* A Bison parser, made by GNU Bison 3.5.1.  */
+/* A Bison parser, made by GNU Bison 2.3.  */
 
-/* Bison interface for Yacc-like parsers in C
+/* Skeleton interface for Bison's Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
-   Inc.
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
 
-   This program is free software: you can redistribute it and/or modify
+   This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,7 +16,9 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -31,72 +33,95 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Undocumented macros, especially those whose name start with YY_,
-   are private implementation details.  Do not rely on them.  */
-
-#ifndef YY_YY_SRC_Y_TAB_H_INCLUDED
-# define YY_YY_SRC_Y_TAB_H_INCLUDED
-/* Debug traces.  */
-#ifndef YYDEBUG
-# define YYDEBUG 0
-#endif
-#if YYDEBUG
-extern int yydebug;
-#endif
-
-/* Token type.  */
+/* Tokens.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-  enum yytokentype
-  {
-    PRINCIPAL = 258,
-    LLAVEIZQ = 259,
-    LLAVEDER = 260,
-    COMA = 261,
-    LISTADE = 262,
-    PROCEDIMIENTO = 263,
-    PARDER = 264,
-    PARIZQ = 265,
-    CORCHIZQ = 266,
-    CORCHDER = 267,
-    IGUAL = 268,
-    INICIOVAR = 269,
-    FINVAR = 270,
-    SI = 271,
-    MIENTRAS = 272,
-    OTROCASO = 273,
-    PARA = 274,
-    HASTA = 275,
-    ITERANDO = 276,
-    HACER = 277,
-    LEER = 278,
-    IMPRIMIR = 279,
-    CADENA = 280,
-    ID = 281,
-    RETROCEDER = 282,
-    DOLLAR = 283,
-    PYC = 284,
-    OP1 = 285,
-    OP2 = 286,
-    OP3 = 287,
-    OP4 = 288,
-    OP5 = 289,
-    OP6 = 290,
-    TIPOS = 291,
-    CONSTANTE = 292
-  };
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     PRINCIPAL = 258,
+     LLAVEIZQ = 259,
+     LLAVEDER = 260,
+     COMA = 261,
+     LISTADE = 262,
+     PROCEDIMIENTO = 263,
+     PARDER = 264,
+     PARIZQ = 265,
+     CORCHIZQ = 266,
+     CORCHDER = 267,
+     IGUAL = 268,
+     INICIOVAR = 269,
+     FINVAR = 270,
+     SI = 271,
+     MIENTRAS = 272,
+     OTROCASO = 273,
+     PARA = 274,
+     HASTA = 275,
+     ITERANDO = 276,
+     HACER = 277,
+     LEER = 278,
+     IMPRIMIR = 279,
+     CADENA = 280,
+     ID = 281,
+     RETROCEDER = 282,
+     DOLLAR = 283,
+     PYC = 284,
+     OP1 = 285,
+     OP2 = 286,
+     OP3 = 287,
+     OP4 = 288,
+     OP5 = 289,
+     OP6 = 290,
+     TIPOS = 291,
+     CONSTANTE = 292
+   };
 #endif
+/* Tokens.  */
+#define PRINCIPAL 258
+#define LLAVEIZQ 259
+#define LLAVEDER 260
+#define COMA 261
+#define LISTADE 262
+#define PROCEDIMIENTO 263
+#define PARDER 264
+#define PARIZQ 265
+#define CORCHIZQ 266
+#define CORCHDER 267
+#define IGUAL 268
+#define INICIOVAR 269
+#define FINVAR 270
+#define SI 271
+#define MIENTRAS 272
+#define OTROCASO 273
+#define PARA 274
+#define HASTA 275
+#define ITERANDO 276
+#define HACER 277
+#define LEER 278
+#define IMPRIMIR 279
+#define CADENA 280
+#define ID 281
+#define RETROCEDER 282
+#define DOLLAR 283
+#define PYC 284
+#define OP1 285
+#define OP2 286
+#define OP3 287
+#define OP4 288
+#define OP5 289
+#define OP6 290
+#define TIPOS 291
+#define CONSTANTE 292
 
-/* Value type.  */
+
+
+
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 typedef int YYSTYPE;
-# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
 #endif
-
 
 extern YYSTYPE yylval;
 
-int yyparse (void);
-
-#endif /* !YY_YY_SRC_Y_TAB_H_INCLUDED  */
