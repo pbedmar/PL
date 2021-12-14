97ff10de1b9c5d8ea2e91673ebb21609897bb769
Mario Carmona Segovia <44337691+Mario-Carmona@users.noreply.github.com>
2021-12-14 16:47:51 +0100

    Restaurado commit sin funciones

--- practica5/src/ejemplo1.c
+++ practica5/src/ejemplo1.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 
-int n, curr;
+int n, d;
 
 int main(int argc, char* argv[])
 {
@@ -16,47 +16,41 @@ int main(int argc, char* argv[])
 	printf("%s", " == ");
 
 	{
-	int temp1 = 2;
-	curr = temp1;
+	int temp1 = 1;
+	d = temp1;
 	}
 
 etiqueta3: ;
-	int temp2 = curr;
+	int temp2 = d;
 	int temp3 = n;
 	bool temp4 = temp2 <= temp3;
 	if (!temp4) goto etiqueta2;
 	{
-		int d;
+		int aux;
 		{
 		int temp5 = n;
-		int temp6 = curr;
-		int temp7 = temp5 / temp6;
-		d = temp7;
+		int temp6 = d;
+		int temp7 = temp5 % temp6;
+		aux = temp7;
 		}
 
-		int temp8 = d;
-		int temp9 = curr;
-		int temp10 = temp8 * temp9;
-		int temp11 = n;
-		bool temp12 = temp10 == temp11;
-		if (!temp12) goto etiqueta1;
+		int temp8 = aux;
+		int temp9 = 0;
+		bool temp10 = temp8 == temp9;
+		if (!temp10) goto etiqueta1;
 		printf("%s", "* ");
-		int temp13 = curr;
-		printf("%i", temp13);
+		int temp11 = d;
+		printf("%i", temp11);
 		printf("%s", " ");
 
-		goto etiqueta0;
 etiqueta1: ;
-		{
-			{
-			int temp14 = curr;
-			int temp15 = 1;
-			int temp16 = temp14 + temp15;
-			curr = temp16;
-			}
 
+		{
+		int temp12 = d;
+		int temp13 = 1;
+		int temp14 = temp12 + temp13;
+		d = temp14;
 		}
-etiqueta0: ;
 
 	}
 	goto etiqueta3;
